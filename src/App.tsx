4b3d8a63343a92910de016e61abc1188bbc23a70
ProgrammingHero1 <programming.hero1@gmail.com>
2026-09-01 15:32:09 -0500

    load countries data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { Suspense } from 'react';
 import './App.css'
+import type { CountryType } from './type';
+import Countries from './components/Countries';
+
+
+// step-1: create a promise to load data 
+const countriesPromise = async():Promise<CountryType[]> =>{
+  const res = await fetch('https://openapi.programming-hero.com/api/all');
+  const data = await res.json();
+  return data.countries;
+}
+
 
 function App() {
-
   return (
     <>
       <h2>World on the GO....</h2>
+      <Suspense fallback={<div>Nadir Loading...</div>}>
+        <Countries countriesPromise={countriesPromise()}></Countries>
+      </Suspense>
     </>
   )
 }
